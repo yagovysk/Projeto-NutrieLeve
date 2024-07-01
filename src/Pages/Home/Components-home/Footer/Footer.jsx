d1a54353b54f50fd72d375a666309fc5ca2ad274
yagovysk <yago09regis@gmail.com>
2024-07-01 12:19:24 -0300

    add

--- src/Pages/Home/Components-home/Footer/Footer.jsx
+++ src/Pages/Home/Components-home/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css";
 export function Footer() {
   return (
     <footer className="footer">
-      <h4>Teste</h4>
+      <h4>&#169; Todos os Direitos Reservados a Nutri e Leve.</h4>
     </footer>
   );
 }
