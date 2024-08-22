b133f35cbb5e21bbbec5a50e488e2d5ef68dda7f
yagovysk <yago09regis@gmail.com>
2024-08-22 11:15:07 -0300

    add

--- src/Pages/Home/Components-home/Marmitas/Marmitas.jsx
+++ src/Pages/Home/Components-home/Marmitas/Marmitas.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaPlus } from "react-icons/fa";
 import { RiSubtractLine } from "react-icons/ri";
 import { IoLogoWhatsapp } from "react-icons/io5";
@@ -9,33 +9,66 @@ export function Marmitas() {
     "I - Filé mignon com molho de mostarda, arroz verde (brócolis, ervilha, salsinha e cebolinha). 350g",
     "II - Frango em cubo ao molho de tomate com grão de bico, arroz integral, cenoura em rodelas. 350g",
     "III - Coxa e sobrecoxa desossada grelhada, arroz com açafrão e banana terra, lentilha refogada. 350g",
-    "IV - IEspaguete sem glúten à bolonhesa, com molho branco e queijo.  350g",
+    "IV - Espaguete sem glúten à bolonhesa, com molho branco e queijo.  350g",
     "V - Filé de frango com molho de ricota, legumes com quinoa vermelha e alho (batata inglesa, abóbora, milho, abobrinha e quinoa). 350g",
     "VI - Moqueca de tilápia, arroz de coco, brócolis no vapor. 350g",
     "VII - Tilápia grelhada, legumes assados, purê de batata. 350g",
-    "VIII - Tilápia com banana da terra gratinada com molho branco, arroz integral, eleta de legumes. 350 g",
+    "VIII - Tilápia com banana da terra gratinada com molho branco, arroz integral, seleta de legumes. 350g",
+  ];
+
+  const caldos = [
+    "Sopa de feijão com patinho moído. 250g",
+    "Sopa de legumes com frango desfiado. 250g",
+    "Canjiquinha com lombo de porco desfiado. 250g",
   ];
 
   const precos = [21.0, 21.0, 21.0, 21.0, 21.0, 26.0, 26.0, 26.0];
+  const precosCaldos = [13.0, 13.0, 13.0];
 
-  const [quantities, setQuantities] = useState([0, 0, 0, 0, 0, 0, 0, 0]);
+  const [quantities, setQuantities] = useState(Array(sabores.length).fill(0));
+  const [quantitiesCaldos, setQuantitiesCaldos] = useState(
+    Array(caldos.length).fill(0)
+  );
+  const [totalPrice, setTotalPrice] = useState(0);
 
-  const handleIncrement = (index) => {
-    const newQuantities = [...quantities];
-    newQuantities[index] += 1;
-    setQuantities(newQuantities);
+  useEffect(() => {
+    const calcularTotal = () => {
+      const totalMarmitas = quantities.reduce(
+        (acc, quantity, index) => acc + quantity * precos[index],
+        0
+      );
+      const totalCaldos = quantitiesCaldos.reduce(
+        (acc, quantity, index) => acc + quantity * precosCaldos[index],
+        0
+      );
+      setTotalPrice(totalMarmitas + totalCaldos);
+    };
+
+    calcularTotal();
+  }, [quantities, quantitiesCaldos]);
+
+  const handleIncrement = (index, type = "marmitas") => {
+    const updateQuantities =
+      type === "marmitas" ? [...quantities] : [...quantitiesCaldos];
+    updateQuantities[index] += 1;
+    type === "marmitas"
+      ? setQuantities(updateQuantities)
+      : setQuantitiesCaldos(updateQuantities);
   };
 
-  const handleDecrement = (index) => {
-    const newQuantities = [...quantities];
-    if (newQuantities[index] > 0) {
-      newQuantities[index] -= 1;
-      setQuantities(newQuantities);
+  const handleDecrement = (index, type = "marmitas") => {
+    const updateQuantities =
+      type === "marmitas" ? [...quantities] : [...quantitiesCaldos];
+    if (updateQuantities[index] > 0) {
+      updateQuantities[index] -= 1;
+      type === "marmitas"
+        ? setQuantities(updateQuantities)
+        : setQuantitiesCaldos(updateQuantities);
     }
   };
 
   const handleOrder = () => {
-    const message = quantities
+    const messageMarmitas = quantities
       .map((quantity, index) => {
         if (quantity > 0) {
           return `${sabores[index]}: ${quantity} x R$${precos[index].toFixed(
@@ -45,15 +78,22 @@ export function Marmitas() {
         return null;
       })
       .filter(Boolean)
-      .join("\n"); // Adiciona quebra de linha entre os itens
+      .join("\n");
 
-    const totalPrice = quantities.reduce(
-      (acc, quantity, index) => acc + quantity * precos[index],
-      0
-    );
+    const messageCaldos = quantitiesCaldos
+      .map((quantity, index) => {
+        if (quantity > 0) {
+          return `${caldos[index]}: ${quantity} x R$${precosCaldos[
+            index
+          ].toFixed(2)} = R$${(quantity * precosCaldos[index]).toFixed(2)}`;
+        }
+        return null;
+      })
+      .filter(Boolean)
+      .join("\n");
 
     const whatsappUrl = `https://wa.me/556199845648?text=${encodeURIComponent(
-      `Olá, gostaria de pedir essa(s) marmita(s):\n\n${message}\n\nTotal: R$${totalPrice.toFixed(
+      `Olá, gostaria de pedir:\n\nMarmitas:\n${messageMarmitas}\n\nCaldos:\n${messageCaldos}\n\nTotal: R$${totalPrice.toFixed(
         2
       )}`
     )}`;
@@ -61,9 +101,8 @@ export function Marmitas() {
   };
 
   const totalQuantity = quantities.reduce((acc, quantity) => acc + quantity, 0);
-
-  const totalPrice = quantities.reduce(
-    (acc, quantity, index) => acc + quantity * precos[index],
+  const totalCaldosQuantity = quantitiesCaldos.reduce(
+    (acc, quantity) => acc + quantity,
     0
   );
 
@@ -71,7 +110,7 @@ export function Marmitas() {
     <section id="marmitas" className="section-marmitas">
       <div className="container-sombra">
         <article className="article-marmitas">
-          <h2>Marmitas da Semana 16/08 a 22/08</h2>
+          <h2>Marmitas da Semana 23/08 a 29/08</h2>
           <h3>Escolha e peça já suas marmitas</h3>
         </article>
         <div className="card-marmitas">
@@ -82,14 +121,38 @@ export function Marmitas() {
               <div className="responsive-button">
                 <button
                   className="button-marmita"
-                  onClick={() => handleDecrement(index)}
+                  onClick={() => handleDecrement(index, "marmitas")}
                 >
                   <RiSubtractLine />
                 </button>
                 <span className="quantity">{quantity}</span>
                 <button
                   className="button-marmita"
-                  onClick={() => handleIncrement(index)}
+                  onClick={() => handleIncrement(index, "marmitas")}
+                >
+                  <FaPlus />
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+        <h3 className="title-caldos">Caldos</h3>
+        <div className="card-marmitas">
+          {quantitiesCaldos.map((quantity, index) => (
+            <div key={index} className="marmita">
+              <p>{caldos[index]}</p>
+              <p>Preço: R${precosCaldos[index].toFixed(2)}</p>
+              <div className="responsive-button">
+                <button
+                  className="button-marmita"
+                  onClick={() => handleDecrement(index, "caldos")}
+                >
+                  <RiSubtractLine />
+                </button>
+                <span className="quantity">{quantity}</span>
+                <button
+                  className="button-marmita"
+                  onClick={() => handleIncrement(index, "caldos")}
                 >
                   <FaPlus />
                 </button>
@@ -99,12 +162,13 @@ export function Marmitas() {
         </div>
         <div className="total">
           <p>Total de marmitas: {totalQuantity}</p>
+          <p>Total de caldos: {totalCaldosQuantity}</p>
           <p>Total a pagar: R${totalPrice.toFixed(2)}</p>
         </div>
         <button
           className="button-order"
           onClick={handleOrder}
-          disabled={totalQuantity === 0}
+          disabled={totalQuantity + totalCaldosQuantity === 0}
         >
           Fazer Pedido <IoLogoWhatsapp />
         </button>
